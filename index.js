02e52201249f6fd09d90cb5ee111f34cfe04b4f9
Hsiao-Ting Yu <sst.dreams@gmail.com>
2018-06-23 06:37:26 +0800

    Add model definition.

--- index.js
+++ index.js
@@ -5,11 +5,23 @@ const sequelize = new Sequelize('homework', 'homework', 'homework', {
   dialect: 'mysql',
   host: 'db',
 });
-const User = sequelize.define('users', {
+const User = sequelize.define('user', {
   facebook_id: Sequelize.BIGINT,
   name: Sequelize.STRING,
 }, {
   timestamps: false,
+});
+const Friend = sequelize.define('friend', {
+  user_id: Sequelize.INTEGER,
+  friend_id: Sequelize.INTEGER,
+}, {
+  timestamps: false,
+});
+User.belongsToMany(User, {
+  as: 'Friends',
+  through: Friend,
+  foreignKey: 'user_id',
+  otherKey: 'friend_id',
 });
 
 
