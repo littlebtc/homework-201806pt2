20e090d5c0cf1960b9cbf6304ee22a5f5995c78f
Hsiao-Ting Yu <sst.dreams@gmail.com>
2018-06-23 06:38:16 +0800

    Finish the first question.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Sequelize = require('sequelize');
 const express = require('express');
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
 
 const sequelize = new Sequelize('homework', 'homework', 'homework', {
   dialect: 'mysql',
@@ -24,7 +26,38 @@ User.belongsToMany(User, {
   otherKey: 'friend_id',
 });
 
-
+const asyncWrapper = fn => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch(next);
+};
 const app = express();
 
+app.get('/', asyncWrapper(async (req, res) => {
+  res.send(JSON.stringify(await User.findAll()));
+}));
+app.get('/p/', (req, res, next) => {
+  User.findAll().then(users => res.send(JSON.stringify(users))).catch(next);
+});
+
+// Parse the Facebook user and write to database.
+app.get('/parse', asyncWrapper(async (req, res) => {
+  const { url } = req.query;
+  const r = await fetch(url);
+  const $ = cheerio.load(await r.text());
+  const profileId = $('meta[property="al:android:url"]').attr('content').match(/[0-9]+/)[0];
+  const name = $('meta[property="og:title"]').attr('content');
+  await User.create({ name, facebook_id: profileId });
+  res.send('ok');
+}));
+app.get('/p/parse', (req, res, next) => {
+  const { url } = req.query;
+  fetch(url).then(r => (r.text())).then((text) => {
+    const $ = cheerio.load(text);
+    const profileId = $('meta[property="al:android:url"]').attr('content').match(/[0-9]+/)[0];
+    const name = $('meta[property="og:title"]').attr('content');
+    return User.create({ name, facebook_id: profileId });
+  }).then(() => {
+    res.send('ok');
+  })
+    .catch(next);
+});
 app.listen(5555);
